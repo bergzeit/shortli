c8e71208723965f3b1cedcfbef78e03c04d414cd
Dennis Stefanovic <dennis.stefanovic@bergzeit.de>
2025-01-29 11:45:32 +0100

    Intitalize Web server with endpoint

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World.")
+}
+
+func shortliTest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Test view.")
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/shortli/test/", shortliTest)
+
+	log.Print("starting server on :4000")
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
